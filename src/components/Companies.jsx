d021377f9450a7be3e9bc696c0a8759f69b7ed53
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-17 22:32:18 +0300

    Companies

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,23 +1,36 @@
-import { Fragment } from 'react';
 import styled from 'styled-components';
 
-const Wrap = styled.div`
+const Flex = styled.div`
   display: flex;
   justify-content: space-between;
 `;
 
+const Wrap = styled.div`
+  margin: 88px 150px;
+  text-align: center;
+`;
+
+const Text = styled.p`
+  color: #bbc3cf;
+  font-weight: 500;
+`;
+
 const Logos = styled.img.attrs((props) => ({
-  src: `/img/${props.children}`,
+  src: `/img/${props.imgSrc}`,
   alt: 'lalala',
 }))``;
 
 export default function Companies() {
   return (
-    <Fragment>
-      <p>Join 4,000+ companies already growing</p>
-      <Wrap>
-        <Logos></Logos>
-      </Wrap>
-    </Fragment>
+    <Wrap>
+      <Text>Join 4,000+ companies already growing</Text>
+      <Flex>
+        <Logos imgSrc='JiggleLogo.svg' />
+        <Logos imgSrc='resecurbLogo.svg' />
+        <Logos imgSrc='SymtricLogo.svg' />
+        <Logos imgSrc='WelyticsLogo.svg' />
+        <Logos imgSrc='WishelpLogo.svg' />
+      </Flex>
+    </Wrap>
   );
 }
